9da8938bfc9b883a1a5670562e06b6d863e8ea6a
thiagobavaresco <thiagobavaresco@unochapeco.edu.br>
2022-08-09 10:51:34 -0300

    Welcome page com lottie

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import AnimatedLottieView from 'lottie-react-native';
+import pokemonAnimation from './pikachu.json'
 
 import * as S from './styles'
 
 export function Welcome(){
     return <S.Container>
-    <S.Content></S.Content>
-    <S.Footer></S.Footer>
+    <S.Content>
+            <AnimatedLottieView autoPlay source={pokemonAnimation} loop />
+    </S.Content>
+    <S.Footer>
+        <S.Tittle>Bem Vindo</S.Tittle>
+        <S.SubTittle>Encontre todos os Pokemons em um só lugar</S.SubTittle>
+    </S.Footer>
     </S.Container>
 }
--- src/pages/Welcome/styles.ts
+++ src/pages/Welcome/styles.ts
@@ -3,11 +3,38 @@ import theme from '../../global/styles/theme';
 
 export const Container = styled.View`
     ${({theme}) => css`
-        felx: 1;
+        flex: 1;
         background-color: ${theme.colors.background}
     `}
 `;
 
-export const Content = styled.View``;
+export const Content = styled.View`
+    height: 70%;
 
-export const Footer = styled.View``;
+`;
+
+export const Footer = styled.View`
+    ${({theme}) => css`
+    justify-content: center;
+    align-items: center
+    border-top-right-radius: 20px;
+    border-top-left-radius: 20px;
+    height: 30%;
+    background-color: ${theme.colors.backgroundCard.water};
+    `}
+`;
+
+export const Tittle = styled.Text`
+    ${({theme}) => css`
+        font-size: 20px;
+        color: ${theme.colors.text}
+    `}
+`;
+
+export const SubTittle = styled.Text`
+    ${({theme}) => css`
+        margin-top: 20px;
+        font-size: 14px;
+        color: ${theme.colors.text}
+    `}
+`;
